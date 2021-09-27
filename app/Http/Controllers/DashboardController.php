26c9f624d9292fdd564a5a5052a462b8af777b2b
Toya1001 <84680956+Toya1001@users.noreply.github.com>
2021-09-27 10:32:05 -0500

    push

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\StudentSelection;
 use App\Models\Course;
+use App\Models\TypesOfCourse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
